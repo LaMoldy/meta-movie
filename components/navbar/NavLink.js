5d484554afde492e2ec2db0d1822bb3e6e32e457
Nikk Jackson <NikkolasJ@Outlook.com>
2022-09-27 12:31:36 -0400

    feat: added spacing around nav links

--- components/navbar/NavLink.js
+++ components/navbar/NavLink.js
@@ -3,11 +3,19 @@ import NextLink from 'next/link';
 
 const NavLink = ({ href, path, display, children }) => {
   const isCurrentPage = href === path; // Checks to see if the url is the same as the path
-  const linkColour = display === 'mobile' ? 'black' : 'white';
 
+  if (display === 'mobile') {
+    return (
+      <NextLink href={href}>
+        <Link color={isCurrentPage ? 'grey' : 'black'} pt={2}>
+          {children}
+        </Link>
+      </NextLink>
+    );
+  }
   return (
     <NextLink href={href}>
-      <Link color={isCurrentPage ? 'grey' : linkColour} mt={7}>
+      <Link color={isCurrentPage ? 'grey' : 'white'} pr={2}>
         {children}
       </Link>
     </NextLink>
