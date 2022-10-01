846a6290df747a819cbae83d9bd97fa86a731904
Nikk Jackson <NikkolasJ@Outlook.com>
2022-10-01 15:28:23 -0400

    feat: added 500 page

---
+++ __tests__/500.test.jsx
@@ -0,0 +1,30 @@
+import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react'
+
+import ServerProblemPage from '../pages/500'
+
+describe('404 Page', () => {
+  it('Image should be present', () => {
+    render(<ServerProblemPage />)
+    const image = screen.getByAltText('500 Image')
+    expect(image).toBeInTheDocument()
+  })
+
+  it('Divider should be present', () => {
+    render(<ServerProblemPage />)
+    const divider = screen.getByTestId('divider')
+    expect(divider).toBeInTheDocument()
+  })
+
+  it('404 header should be present', () => {
+    render(<ServerProblemPage />)
+    const header = screen.getByTestId('500')
+    expect(header).toBeInTheDocument()
+  })
+
+  it('Lost text should be present', () => {
+    render(<ServerProblemPage />)
+    const text = screen.getByTestId('server-text')
+    expect(text).toBeInTheDocument()
+  })
+})
---
+++ pages/500.js
@@ -0,0 +1,34 @@
+import { Container, Divider, Flex, Heading, Text } from '@chakra-ui/react'
+import Image from 'next/image'
+
+const ServerProblemPage = () => {
+  return (
+    <Flex alignContent="center" justifyContent="center" flexDir="column">
+      <Container
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+        textAlign="center"
+        gap={2}
+        height="70vh"
+      >
+        <Image
+          src="/sad_face.png"
+          width="100px"
+          height="100px"
+          alt="500 Image"
+        />
+        <Divider orientation="horizontal" data-testid="divider" />
+        <Heading as="h1" data-testid="500">
+          500
+        </Heading>
+        <Text data-testid="server-text">
+          Looks like the server has run into a problem!
+        </Text>
+      </Container>
+    </Flex>
+  )
+}
+
+export default ServerProblemPage
